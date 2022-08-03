6f756d343a9ae402769add8ae7e61c05a1adf317
Harshaadithya <harshaadithya.20is@kct.ac.in>
2022-08-03 11:35:27 +0530

    requirements.txt added

--- info/models.py
+++ info/models.py
@@ -19,7 +19,7 @@ class Service(models.Model):
 class Testimonial(models.Model):
     client_name=models.CharField(max_length=60)
     client_organisation=models.CharField(max_length=30)
-    client_position=models.CharField(max_length=30)
+    client_position=models.CharField(max_length=30,null=True,blank=True)
     client_review=models.CharField(max_length=500)
     client_profile_img=models.ImageField(default='client_profile_img/profile_img.png',upload_to='client_profile_img/')
     created_at=models.DateTimeField(auto_now_add=True) 
