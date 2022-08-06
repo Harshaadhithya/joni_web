a4a80256f45dccf6d51ae5a77fd73bf056d1f696
Harshaadithya <harshaadithya.20is@kct.ac.in>
2022-08-06 12:53:21 +0530

    download excel

--- jonisolutions/settings.py
+++ jonisolutions/settings.py
@@ -127,8 +127,11 @@ STATICFILES_DIRS=[os.path.join(BASE_DIR,'static')]
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
 
-MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
-MEDIA_URL='/images/'
+# MEDIA_ROOT=os.path.join(BASE_DIR,'static/images')
+# MEDIA_URL='/images/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  
